77deb4d848eac3fdd12f0e909737c34ab7fbc7c0
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:28 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedHeader.foundation.mjs
@@ -0,0 +1,13 @@
+
+/*! Foundation styling wrapper for FixedHeader
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-zf';
+import DataTable from 'datatables.net-fixedheader';
+
+
+
+
+export default DataTable;
